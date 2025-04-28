8453a0fa85a0afc8248de433db6291658b17d621
sungchan12 <chanyouunng1@gmail.com>
2025-04-28 19:21:54 +0900

    Add files via upload

---
+++ PetTama/src/main/java/com/example/PetTama/PetTamaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetTama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTamaApplication.class, args);
+	}
+
+}
---
+++ PetTama/src/test/java/com/example/PetTama/PetTamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetTama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
