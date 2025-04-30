44998746cb9f8d534c2ab6d8a9d7856f0350dbf7
sungchan12 <chanyouunng1@gmail.com>
2025-04-30 10:16:01 +0900

    Add files via upload

---
+++ PetTama/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into users(username, password, email) values ('찬영', '1234', 'test123@gmail.com');
+insert into users(username, password, email) values ('인택', '1234', 'test1234@gmail.com');
+insert into users(username, password, email) values ('찬일', '1234', 'test1235@gmail.com');
+insert into users(username, password, email) values ('아웃택', '1234', 'test1236@gmail.com');
+
+INSERT INTO pet (user_id, hunger, sleepiness, happiness, last_updated) VALUES (1,50, 50, 60, '2025-04-29 23:00:00');
+INSERT INTO pet (user_id, hunger, sleepiness, happiness, last_updated) VALUES (2,51, 52, 61, '2025-04-29 23:00:00');
+INSERT INTO pet (user_id, hunger, sleepiness, happiness, last_updated) VALUES (3,52, 54, 62, '2025-04-29 23:00:00');
+INSERT INTO pet (user_id, hunger, sleepiness, happiness, last_updated) VALUES (4,53, 56, 63, '2025-04-29 23:00:00');
+INSERT INTO pet (user_id, hunger, sleepiness, happiness, last_updated) VALUES (1,54, 58, 64, '2025-04-29 23:00:00');
+INSERT INTO pet (user_id, hunger, sleepiness, happiness, last_updated) VALUES (2,55, 60, 65, '2025-04-29 23:00:00');
+INSERT INTO pet (user_id, hunger, sleepiness, happiness, last_updated) VALUES (3,56, 62, 70, '2025-04-29 23:00:00');
+INSERT INTO pet (user_id, hunger, sleepiness, happiness, last_updated) VALUES (4,57, 64, 68, '2025-04-29 23:00:00');
