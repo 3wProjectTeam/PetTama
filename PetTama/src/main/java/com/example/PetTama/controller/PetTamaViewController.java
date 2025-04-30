3d56194e0d6b7baf6d26ca4cbf3d7744cc6f8956
sungchan12 <chanyouunng1@gmail.com>
2025-04-30 22:31:26 +0900

    4/30 View

---
+++ PetTama/src/main/java/com/example/PetTama/controller/PetTamaViewController.java
@@ -0,0 +1,13 @@
+package com.example.PetTama.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PetTamaViewController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "index";
+    }
+}
